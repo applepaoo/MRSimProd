01248b658845de753e71a0374b1481989fe6d75f
applepaoo <applepaoo@gmail.com>
2018-08-15 15:01:34 +0800

    modified

--- src/tw/com/ruten/ts/mapreduce/JImageHashClusterSample.java
+++ src/tw/com/ruten/ts/mapreduce/JImageHashClusterSample.java
@@ -30,13 +30,11 @@ import org.json.simple.parser.ParseException;
 import tw.com.ruten.ts.utils.JobUtils;
 import tw.com.ruten.ts.utils.TsConf;
 
-
 /**
  * 
  * cluster sample
  * 
- * input text json input
- * output text 
+ * input text json input output text
  * 
  * @author realmeat
  *
@@ -47,31 +45,30 @@ public class JImageHashClusterSample extends Configured implements Tool {
 	private static String CLUSTERING_FORMAT = "clustering.file.format";
 	public Configuration conf;
 
-	//public static Text HASH_KEY;
 	public static Text G_NO = new Text("G_NO");
-	
+
 	public static class JImageHashClusterMapper extends Mapper<LongWritable, Text, SortedKey, MapWritable> {
 		private Configuration conf;
 		private JSONParser parser = new JSONParser();
-		// private Text outKey = new Text();
 		String hashKey = null;
 		int bitCount = 0;
 		int bitShift = 0;
 		BigInteger mask;
+
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
 			conf = context.getConfiguration();
-			hashKey = conf.get("cluster.hashkey","IMG_HASH_V1");
+			hashKey = conf.get("cluster.hashkey", "IMG_HASH_V1");
 			bitCount = conf.getInt("cluster.bitcount", 256);
-			bitShift = conf.getInt("cluster.bitshift", 64);
-			
+			bitShift = conf.getInt("cluster.bitshift", 32);
+
 			StringBuilder strMask = new StringBuilder();
-			
-			for(int i=0; i<bitShift; i++) {
+
+			for (int i = 0; i < bitShift; i++) {
 				strMask.append('1');
 			}
-			
-			mask = new BigInteger(strMask.toString(),2);
+
+			mask = new BigInteger(strMask.toString(), 2);
 		}
 
 		@Override
@@ -84,31 +81,29 @@ public class JImageHashClusterSample extends Configured implements Tool {
 
 				MapWritable outValue = new MapWritable();
 				Set<Object> keySet = jsonObject.keySet();
-				for(Object tmpKey: keySet) {
+				for (Object tmpKey : keySet) {
 					Object tmpValue = jsonObject.get(tmpKey);
-					
+
 					outValue.put(new Text(tmpKey.toString()), new Text(tmpValue.toString()));
 				}
 
-				// outValue.put(new Text("G_NO"), new Text(jsonObject.get("G_NO").toString()));
-
 				String hash = jsonObject.get(hashKey).toString();
 				BigInteger tmp = hex2BigInt(hash);
-				
+
 				List<BigInteger> list = new ArrayList<BigInteger>();
-				for(int i=0; i<bitCount; i+=bitShift) {
-					BigInteger t = tmp.shiftRight(i); 
+				for (int i = 0; i < bitCount; i += bitShift) {
+					BigInteger t = tmp.shiftRight(i);
 					list.add(t.and(mask));
 				}
-				
-				for(BigInteger bigInt: list) { /// spread
+
+				for (BigInteger bigInt : list) { /// spread
 					String hex = bigInt2Hex(bigInt);
 					SortedKey outKey = new SortedKey();
 					outKey.defaultKey.set(hex);
 					outKey.sortValue.set(hash);
 					context.write(outKey, outValue);
 				}
-				
+
 				context.getCounter("Mapper", "out").increment(1);
 
 			} catch (ParseException e) {
@@ -116,44 +111,44 @@ public class JImageHashClusterSample extends Configured implements Tool {
 				context.getCounter("Mapper", "parse.exception").increment(1);
 			}
 		}
-		
+
 	}
 
 	public static class JImageHashClusterReducer extends Reducer<SortedKey, MapWritable, Text, Text> {
 		private Configuration conf;
 		int threshold = 0;
-		Text targetHash = new Text();	
-		
+		Text targetHash = new Text();
+
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
 			conf = context.getConfiguration();
-			threshold = conf.getInt("cluster.threshold", 128);
-			targetHash.set(conf.get("cluster.hashkey","IMG_HASH_V1"));
+			threshold = conf.getInt("cluster.threshold", 10);
+			targetHash.set(conf.get("cluster.hashkey", "IMG_HASH_V1"));
 		}
 
 		public void reduce(SortedKey key, Iterable<MapWritable> values, Context context)
 				throws IOException, InterruptedException {
 
-			//newkey.set(hexToBin(String.valueOf(key.defaultKey)));
+			// newkey.set(hexToBin(String.valueOf(key.defaultKey)));
 			String preHashKey = null;
 			String preGno = null;
 			boolean hasSim = false;
 			for (MapWritable val : values) {
 				String hashKey = val.get(targetHash).toString();
 				String gno = val.get(G_NO).toString();
-						
-				
-				if(preHashKey == null) {
+
+				if (preHashKey == null) {
 					preHashKey = hashKey;
 					preGno = gno;
-				}else {
-					if(dis(hashKey,preHashKey) < threshold) {
+				} else {
+					if (dis(hashKey, preHashKey) < threshold) {
 						context.write(new Text(gno), new Text(preHashKey));
-					}else {
-						if(hasSim) {
+						hasSim = true;
+					} else {
+						if (hasSim) {
 							context.write(new Text(preGno), new Text(preHashKey));
 						}
-						
+
 						preHashKey = hashKey;
 						preGno = gno;
 						hasSim = false;
@@ -167,19 +162,20 @@ public class JImageHashClusterSample extends Configured implements Tool {
 		protected void cleanup(Context context) throws IOException, InterruptedException {
 		}
 	}
-	
-	static BigInteger hex2BigInt(String in) { 
+
+	static BigInteger hex2BigInt(String in) {
 		return new BigInteger(in, 16);
 	}
 
 	static String bigInt2Hex(BigInteger in) {
 		return in.toString(16);
-	
+
 	}
+
 	static int dis(BigInteger in1, BigInteger in2) { // hamming distance
 		return in1.xor(in2).bitCount();
 	}
-	
+
 	static int dis(String in1, String in2) {
 		return dis(hex2BigInt(in1), hex2BigInt(in2));
 	}
